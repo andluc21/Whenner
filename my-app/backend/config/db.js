0b9f61de0876438662a0a6acdbd81c7f15d85e2b
luca <luca627@comcast.net>
2020-09-16 11:03:18 -0700

    updates user features

--- my-app/backend/config/db.js
+++ my-app/backend/config/db.js
@@ -4,14 +4,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://whenner:agoodpass505@whennerappcluster.n5tz7.mongodb.net/test",
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
